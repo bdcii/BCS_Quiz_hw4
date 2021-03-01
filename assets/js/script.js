a4e01e47fdeac0320e12ac026ef4a87a1a22ac5c
bdcii <darrell.chess@gmail.com>
2021-03-01 17:19:47 -0500

    updated js since it was not originally working.  after working with tutor, I was able to simplify my code!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 //required elements
 const start = document.querySelector(".start");
-const questionContainer = document.querySelector(".container");
+var questionContainer = document.querySelector(".container");
 const results_box = document.querySelector(".results_container");
 
 
 start.onclick = startGame(){
     questionContainer.classList.add(".activeInfo");
 
-}
-start.addEventListener("click", startGame());
+};
+
+
    
 
 
