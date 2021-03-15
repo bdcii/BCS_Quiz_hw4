961b63f04897bc8cd1cea5ebe91fe4c1b504a536
bdcii <darrell.chess@gmail.com>
2021-03-15 12:53:16 -0400

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,46 @@
 //Setting variables for my functions
-const rules = document.querySelector(".info_card")
-const start = document.querySelector(".start");
-const questionContainer = document.querySelector(".container");
-const results_box = document.querySelector(".results_container");
+var rules = document.querySelector(".info_card")
+var start = document.querySelector("#start_btn");
+var questionContainer = document.querySelector(".container");
+var results_box = document.querySelector(".results_container");
+
+var timerEl = document.querySelector(".timer");
+
+var secondsLeft = 31;
 
 
-//Start Game//
-function startGame(){
-    rules.classList.add("invisible");
-    questionContainer.classList.remove("invisible");
-    questionContainer.classList.add("visible");
+
+//function to set the timer
+function setTimer(){
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+
+            //create function to show results page and user score
+        }
+    }, 1000);
 }
 
-start.addEventListener("click", startGame());
-   
+
+
+
+//code runs when startGame is initiated
+function startGame(){
+    rules.classList.add("invisible");
+    rules.classList.remove("visible");
+    questionContainer.classList.remove("invisible");
+    questionContainer.classList.add("visible");
+    results_box.classList.remove("visible");
+    results_box.classList.add("invisible"); 
+}
+
+start.addEventListener("click", function() {
+    setTimer();
+   //code to start timer
+   startGame();
+});
 
 
